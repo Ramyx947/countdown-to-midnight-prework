9b69fc22a59565773704146f7a6bedaa5b488660
Ramona M <r1mari@yahoo.com>
2018-06-28 11:16:48 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(countdown_output)
   countdown_output=10
   while countdown_output>0
-  puts " #{countdown_output} SECOND(S)!\n"
+  prints "#{countdown_output} SECOND(S)!"
   countdown_output-=1
 end
   puts "HAPPY NEW YEAR!"
